59c9ed978c8cb54b6ff293e47488732830e11421
jkthomas <tom.januszek@gmail.com>
2018-10-27 19:15:13 +0200

    [VIEW] Integrated with Kivy. Created display of initial ship positions generation.

--- src/main.py
+++ src/main.py
@@ -1,12 +1,28 @@
+from kivy.uix.screenmanager import Screen
+
 import Generation.Generate
 import numpy as np
+from kivy.app import App
+from kivy.lang import Builder
+from View import GameScreen
+
+Builder.load_file('Battleships.kv')
 
 
-def main():
-    generator = Generation.Generate.Generate()
-    gameboard = generator.generate_all_ships_positions()
-    print(np.matrix(gameboard))
+class BattleshipsApp(App):
+
+    def build(self):
+        return GameScreen.GameScreen()
 
 
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    BattleshipsApp().run()
+
+# def main():
+#     generator = Generation.Generate.Generate()
+#     gameboard = generator.generate_all_ships_positions()
+#     print(np.matrix(gameboard))
+#
+#
+# if __name__ == "__main__":
+#     main()
